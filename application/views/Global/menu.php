8c7705a08bedfac20e787093b6df7ff1a2873826
Frida <fridamzt26@gmail.com>
2019-06-20 11:53:12 -0600

    Inicio

---
+++ application/views/Global/menu.php
@@ -0,0 +1,21 @@
+<!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-right info">
+          <p>Propuesta Aaron</p>
+        </div>
+        <div class="clear">&nbsp;</div>
+      </div>
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">CATALOGOS</li>
+          <!--Menu-->
+          <li><a href="<?php echo base_url();?>index.php/Inicio/formulario"><i class="fa fa-file"></i> <span>Titulo Electronico</span></a></li>
+      	 
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
